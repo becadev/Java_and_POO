8d588f3098c199c8ef590f61bcc9fa58e1e246a5
Rebeca Noemi <rebecanoemi494@gmail.com>
2024-10-26 14:45:12 -0300

    Update Validacao_nota.java

--- Aula4_lacos/Exercicios_beecrowd/Validacao_nota.java
+++ Aula4_lacos/Exercicios_beecrowd/Validacao_nota.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 
 public class Validacao_nota {
         public static void main(String[] args) {
-            double nota1 = 11.00, nota2 = 11.00;
-
-            while ((nota1 < 0 || nota1 > 10) || (nota2 < 0 || nota2 > 10)){
-                Scanner entrada = new Scanner(System.in);
+            double nota1, nota2, media;
+            Scanner entrada = new Scanner(System.in);
+            nota1 = entrada.nextDouble();
+            while (nota1 < 0 || nota1 > 10) {
+                System.out.println("nota invalida");
                 nota1 = entrada.nextDouble();
-                nota2 = entrada.nextDouble();
-
-                if ((nota1 < 0 || nota1 > 10) || (nota2 < 0 || nota2 > 10)){
-                    System.out.print("nota invalida");
             }
-        }
+            nota2 = entrada.nextDouble();
+            while (nota2 < 0 || nota2 > 10) {
+                System.out.println("nota invalida");
+                nota2 = entrada.nextDouble();
+            }
+            media = (nota1+nota2)/2;
+            System.out.printf("media = %.2f\n", media);
     }
 }
