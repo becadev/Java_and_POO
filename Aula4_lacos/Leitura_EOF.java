0bcbda97a2d066feb845d050df78ed833646db50
Rebeca Noemi <rebecanoemi494@gmail.com>
2024-10-26 14:43:40 -0300

    Create  Leitura_EOF.java

---
+++ Aula4_lacos/Leitura_EOF.java
@@ -0,0 +1,21 @@
+package Aula4_lacos;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Leitura_EOF {
+        public static void main(String[] args) {
+            try{
+                File arquivo = new File("Aula4_lacos/Exercicios_beecrowd/Teste.txt");
+                Scanner sc = new Scanner(arquivo);
+                while(sc.hasNext()){
+                    String linha = sc.nextLine();
+                    System.out.println(linha);
+            }
+            sc.close();
+        } catch (FileNotFoundException e){
+            System.out.println("Arquivo não encontrado" + e.getMessage());
+        }
+    }
+}
